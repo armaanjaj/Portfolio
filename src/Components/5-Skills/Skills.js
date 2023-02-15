2adfdd3b92eb061e3b924bba076a314c1e9837d2
armaanjaj <armaan.jaj@gmail.com>
2023-02-15 00:21:48 -0700

    delete old react files

--- src/Components/5-Skills/Skills.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Skills() {
-  return (
-    <div className='skills-container-p'>
-      This is skills section
-    </div>
-  )
-}
--- src/Content/Hero.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    greeting : "Hi, my name is",
-    name : "Armaan Jaj",
-    headline : "A fullStack web developer",
-    content : "I'm a full stack software developer who enjoys developing web application using MERN stack. I specialize in building and designing digital adventures using React.js, Express.js, Node.js, MongoDB along with GraphQL."
-}
